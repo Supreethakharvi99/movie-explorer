d630fd5b41a079f63a5d12a856a3e51d8123a746
Supreetha Kharvi <suprithakharvi99@gmail.com>
2026-09-07 22:21:16 +0530

    favorites created

---
+++ src/components/Favorites.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Favorites = () => {
+  return (
+    <div className="mx-auto max-w-7xl px-6 py-8">
+        <h1 className='text-2xl font-bold text-slate-900'>
+            My Favorites
+        </h1>
+
+        <p className='mt-2 text-slate-500'>
+            Your favorite movies will appear here.
+        </p>
+    </div>
+  )
+}
+
+export default Favorites
